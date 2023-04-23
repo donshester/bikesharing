1c845af9502349b67f183516b400e38eade4c9e8
Vladislav Stepanov <vlad.stepanov.2003@bk.ru>
2023-04-23 20:38:24 +0300

    FEAT: fixed guard to implementation RBAC

--- src/user/guards/user.guard.ts
+++ src/user/guards/user.guard.ts
@@ -3,18 +3,25 @@ import {
   ExecutionContext,
   HttpException,
   HttpStatus,
-  Injectable,
+  Injectable, UnauthorizedException,
 } from '@nestjs/common';
 import { ExpressRequest } from '../../../types/expressRequest.interface';
+import {Reflector} from "@nestjs/core";
+import {Roles} from "../types/roles.enum";
 
 @Injectable()
 export class UserGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest<ExpressRequest>();
-
-    if (request.user) {
+  constructor(private readonly reflector: Reflector) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRole = this.reflector.get<Roles>('role', context.getHandler());
+    if(!requiredRole){
       return true;
     }
-    throw new HttpException('Not authorized', HttpStatus.UNAUTHORIZED);
+
+    const request = context.switchToHttp().getRequest<ExpressRequest>();
+    if (!request.user || (request.user.role !== requiredRole)) {
+      throw new UnauthorizedException('You are not authorized to access this resource');
+    }
+    return true;
   }
 }
