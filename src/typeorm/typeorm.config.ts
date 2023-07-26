464026a2415d548afa60f7514fee0ece950aa79d
Vladislav Stepanov <vlad.stepanov.2003@bk.ru>
2023-07-27 01:20:32 +0300

    db connection with pg

--- src/typeorm/typeorm.config.ts
+++ src/typeorm/typeorm.config.ts
@@ -8,16 +8,18 @@ config({ path: join(process.cwd(), '.env') });
 const configService = new ConfigService();
 
 const options = (): DataSourceOptions => {
+  const url = configService.get('DATABASE_URL');
+  if (!url) {
+    throw new Error('Database url not found');
+  }
+  console.log(url);
   return {
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(<string>process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    url: url,
     type: 'postgres',
     logging: configService.get('IS_PROD') === 'false',
     entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
     migrations: ['dist/migrations/*.{ts,js}'],
+    synchronize: true,
   };
 };
 export const appDataSource = new DataSource(options());
